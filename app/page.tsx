416204a3ad81eb1dbb59f839ef5ed5509228c5e1
Mayank <mayank.nice9411@gmail.com>
2023-05-06 18:57:33 +0530

    first commit1

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
