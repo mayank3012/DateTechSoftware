140246e039c831bf542ae474612ba930773b2440
Mayank <mayank.nice9411@gmail.com>
2023-05-06 18:50:03 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Contact />
+    </>
+  );
+}
